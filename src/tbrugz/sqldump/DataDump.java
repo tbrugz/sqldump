c158c1d73e5c6116fff21beadf37d50eb776ba52
tbrugz <none@none>
2011-06-10 12:25:00 -0300

    datadump: adding sqldump.datadump.recorddelimiter and sqldump.datadump.columndelimiter properties

--- src/tbrugz/sqldump/DataDump.java
+++ src/tbrugz/sqldump/DataDump.java
@@ -15,13 +15,21 @@ public class DataDump {
 
 	static final String PROP_DATADUMP_FILEPATTERN = "sqldump.datadump.filepattern";
 	static final String PROP_DATADUMP_TABLENAMEHEADER = "sqldump.datadump.tablenameheader";
-
+	static final String PROP_DATADUMP_RECORDDELIMITER = "sqldump.datadump.recorddelimiter";
+	static final String PROP_DATADUMP_COLUMNDELIMITER = "sqldump.datadump.columndelimiter";
+	
+	static final String DELIM_RECORD_DEFAULT = "\n";
+	static final String DELIM_COLUMN_DEFAULT = ";";
+	
 	static final String FILENAME_PATTERN_TABLENAME = "\\$\\{tablename\\}";
 	
 	static Logger log = Logger.getLogger(SQLDump.class);
 	
 	void dumpData(Connection conn, List<String> tableNamesForDataDump, Properties prop) throws Exception {
 		log.info("data dumping...");
+		
+		String recordDelimiter = prop.getProperty(PROP_DATADUMP_RECORDDELIMITER, DELIM_RECORD_DEFAULT);
+		String columnDelimiter = prop.getProperty(PROP_DATADUMP_COLUMNDELIMITER, DELIM_COLUMN_DEFAULT);
 		
 		for(String table: tableNamesForDataDump) {
 			String filename = prop.getProperty(PROP_DATADUMP_FILEPATTERN);
@@ -32,12 +40,12 @@ public class DataDump {
 			log.info("dumping data from table: "+table);
 			ResultSet rs = st.executeQuery("select * from \""+table+"\"");
 			if("true".equals(prop.getProperty(PROP_DATADUMP_TABLENAMEHEADER, "false"))) {
-				out("[table "+table+"]", fos);
+				out("[table "+table+"]", fos, recordDelimiter);
 			}
 			ResultSetMetaData md = rs.getMetaData();
 			int numCol = md.getColumnCount();
 			while(rs.next()) {
-				out(SQLUtils.getRowFromRS(rs, numCol, table), fos);
+				out(SQLUtils.getRowFromRS(rs, numCol, table, columnDelimiter), fos, recordDelimiter);
 			}
 			rs.close();
 			
@@ -46,8 +54,8 @@ public class DataDump {
 		
 	}
 	
-	void out(String s, FileWriter fos) throws IOException {
-		fos.write(s+"\n");
+	void out(String s, FileWriter fos, String recordDelimiter) throws IOException {
+		fos.write(s+recordDelimiter);
 	}
 	
 }
--- src/tbrugz/sqldump/SQLUtils.java
+++ src/tbrugz/sqldump/SQLUtils.java
@@ -8,10 +8,14 @@ public class SQLUtils {
 	static StringBuffer sbTmp = new StringBuffer();
 
 	public static String getRowFromRS(ResultSet rs, int numCol, String table) throws SQLException {
+		return getRowFromRS(rs, numCol, table, ";");
+	}
+	
+	public static String getRowFromRS(ResultSet rs, int numCol, String table, String delimiter) throws SQLException {
 		sbTmp.setLength(0);
 		for(int i=1;i<=numCol;i++) {
 			sbTmp.append(rs.getString(i));
-			sbTmp.append(";");
+			sbTmp.append(delimiter);
 		}
 		return sbTmp.toString();
 	}
