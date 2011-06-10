46a76c82453922710c21f01ad9a4d76a3b1a1e08
tbrugz <none@none>
2011-06-10 12:11:05 -0300

    adding datadump output filename pattern: ${tablename}

--- src/tbrugz/sqldump/DataDump.java
+++ src/tbrugz/sqldump/DataDump.java
@@ -14,27 +14,36 @@ import org.apache.log4j.Logger;
 public class DataDump {
 
 	static final String PROP_DATADUMP_FILEPATTERN = "sqldump.datadump.filepattern";
+	static final String PROP_DATADUMP_TABLENAMEHEADER = "sqldump.datadump.tablenameheader";
 
+	static final String FILENAME_PATTERN_TABLENAME = "\\$\\{tablename\\}";
+	
 	static Logger log = Logger.getLogger(SQLDump.class);
 	
 	void dumpData(Connection conn, List<String> tableNamesForDataDump, Properties prop) throws Exception {
-		FileWriter fos = new FileWriter(prop.getProperty(PROP_DATADUMP_FILEPATTERN));
 		log.info("data dumping...");
 		
 		for(String table: tableNamesForDataDump) {
+			String filename = prop.getProperty(PROP_DATADUMP_FILEPATTERN);
+			filename = filename.replaceAll(FILENAME_PATTERN_TABLENAME, table);
+			FileWriter fos = new FileWriter(filename);
+			
 			Statement st = conn.createStatement();
 			log.info("dumping data from table: "+table);
 			ResultSet rs = st.executeQuery("select * from \""+table+"\"");
-			out("\n[table "+table+"]\n", fos);
+			if("true".equals(prop.getProperty(PROP_DATADUMP_TABLENAMEHEADER, "false"))) {
+				out("[table "+table+"]", fos);
+			}
 			ResultSetMetaData md = rs.getMetaData();
 			int numCol = md.getColumnCount();
 			while(rs.next()) {
 				out(SQLUtils.getRowFromRS(rs, numCol, table), fos);
 			}
 			rs.close();
+			
+			fos.close();
 		}
 		
-		fos.close();
 	}
 	
 	void out(String s, FileWriter fos) throws IOException {
