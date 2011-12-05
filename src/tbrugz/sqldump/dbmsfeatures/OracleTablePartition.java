247d41ac39b1eccd4f12480e451c1e455dcbfb18
tbrugz <none@none>
2011-12-05 17:38:54 -0200

    oops: added OracleTablePartition

---
+++ src/tbrugz/sqldump/dbmsfeatures/OracleTablePartition.java
@@ -0,0 +1,21 @@
+package tbrugz.sqldump.dbmsfeatures;
+
+import java.util.List;
+
+// http://download.oracle.com/docs/cd/B28359_01/server.111/b28286/statements_7002.htm#i2125922
+public class OracleTablePartition {
+	public String name;
+	public String tableSpace;
+
+	/* range_values_clause::= */
+	//TODO: remove this: keep only 'values'
+	public List<String> upperValues;
+	//add maxvalue?
+	
+	/* list_values_clause::= */
+	public List<String> values;
+	//add null? DEFAULT?
+
+	/* individual_hash_partitions::= */
+	//nothing, just name...
+}
