25653d27b1eb81821c2f05a8cdd10def0912a55e
tbrugz <none@none>
2012-07-04 18:11:42 +0100

    processors: StatsProc: cleanup

--- src_xtra/tbrugz/sqldump/util/MapEntryValueComparator.java
+++ src_xtra/tbrugz/sqldump/util/MapEntryValueComparator.java
@@ -12,7 +12,7 @@ import java.util.Map.Entry;
 /**
  * @see: http://stackoverflow.com/questions/109383/how-to-sort-a-mapkey-value-on-the-values-in-java
  */
-public class MapEntryValueComparator implements Comparator<Map.Entry> {
+public class MapEntryValueComparator<K,V> implements Comparator<Map.Entry<K,V>> {
 	public MapEntryValueComparator() {}
 	public MapEntryValueComparator(boolean inverse) {
 		this.inverse = inverse;
@@ -21,29 +21,29 @@ public class MapEntryValueComparator implements Comparator<Map.Entry> {
 	boolean inverse = false;
 	
 	@Override
-	public int compare(Entry o1, Entry o2) {
+	public int compare(Entry<K,V> o1, Entry<K,V> o2) {
 		if(inverse) {
-			return ((Comparable) o2.getValue())
+			return ((Comparable<V>) o2.getValue())
 					.compareTo(o1.getValue());
 		}
-		return ((Comparable) o1.getValue())
+		return ((Comparable<V>) o1.getValue())
 				.compareTo(o2.getValue());
 	}
 
-	public static Map sortByValue(Map map) {
+	public static <K,V> Map<K,V> sortByValue(Map<K,V> map) {
 		return sortByValue(map, false);
 	}
 
-	public static Map sortByValue(Map map, boolean inverse) {
-		List list = new LinkedList(map.entrySet());
-		Collections.sort(list, new MapEntryValueComparator(inverse));
+	public static <K,V> Map<K,V> sortByValue(Map<K,V> map, boolean inverse) {
+		List<Map.Entry<K,V>> list = new LinkedList<Map.Entry<K,V>>(map.entrySet());
+		Collections.sort(list, new MapEntryValueComparator<K,V>(inverse));
 
-		Map result = new LinkedHashMap();
-		for (Iterator it = list.iterator(); it.hasNext();) {
-			Map.Entry entry = (Map.Entry)it.next();
+		Map<K,V> result = new LinkedHashMap<K,V>();
+		for (Iterator<Map.Entry<K,V>> it = list.iterator(); it.hasNext();) {
+			Map.Entry<K,V> entry = it.next();
 			result.put(entry.getKey(), entry.getValue());
 		}
 		return result;
-	} 
+	}
 	
 }
--- src_xtra/tbrugz/sqldump/xtraproc/StatsProc.java
+++ src_xtra/tbrugz/sqldump/xtraproc/StatsProc.java
@@ -15,33 +15,6 @@ import tbrugz.sqldump.util.MapEntryValueComparator;
 public class StatsProc extends AbstractSQLProc {
 
 	static Log log = LogFactory.getLog(StatsProc.class);
-
-	//Map<String, Integer> mapCounts = new HashMap<String, Integer>();
-	
-	/*public static class ValueComparator implements Comparator<Object> {
-
-		Map<?,?> base;
-		public ValueComparator(Map<?,?> base) {
-			this.base = base;
-		}
-
-		public int compare(Object a, Object b) {
-			Object va = base.get(a);
-			Object vb = base.get(b);
-			log.warn("comp: "+a+":"+b+" / "+va+":"+vb);
-			//XXX: not good if compare returns 0 (no partial order)
-			if(va instanceof Comparable) {
-				return ((Comparable) va).compareTo(vb);
-			}
-			log.warn("err: "+a+":"+b+" / "+va+":"+vb);
-			
-			return 0;
-		}
-	}*/
-	
-	
-	/*public static Set sortMapKeysByValue(Map map, Comparator comp) {
-	}*/
 	
 	@Override
 	public void process() {
@@ -64,9 +37,6 @@ public class StatsProc extends AbstractSQLProc {
 			System.out.println("map key/value: " + key + "/"+map.get(key));
 		}*/
 
-		//ValueComparator bvc =  new ValueComparator(map);
-		//Map<String,Integer> sorted_map = new TreeMap<String,Integer>(bvc);
-		//sorted_map.putAll(map);
 		Map<String,Integer> sorted_map = MapEntryValueComparator.sortByValue(map, true);
 		
 		for (String key: sorted_map.keySet()) {
