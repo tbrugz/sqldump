e82b940c5076bd639de3f69b2a2d8bc6b73a151f
tbrugz <none@none>
2012-12-06 19:02:51 +0000

    test: added ParametrizedPropertiesTest

---
+++ src_test/tbrugz/sqldump/util/ParametrizedPropertiesTest.java
@@ -0,0 +1,47 @@
+package tbrugz.sqldump.util;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParametrizedPropertiesTest {
+	
+	public static final String path = "/tbrugz/sqldump/util/";
+	
+	ParametrizedProperties pp = null;
+	
+	@Before
+	public void setup() {
+		pp = new ParametrizedProperties();
+		ParametrizedProperties.setUseSystemProperties(false);
+	}
+	
+	@Test
+	public void testP1() throws IOException {
+		pp.load(ParametrizedProperties.class.getResourceAsStream(path+"p1.properties"));
+		Assert.assertEquals("value1", pp.getProperty("id1"));
+	}
+
+	@Test
+	public void testP2Include() throws IOException {
+		pp.load(ParametrizedProperties.class.getResourceAsStream(path+"p2.properties"));
+		Assert.assertEquals("value1", pp.getProperty("id1"));
+	}
+
+	@Test
+	public void testP3() throws IOException {
+		pp.load(ParametrizedProperties.class.getResourceAsStream(path+"p3.properties"));
+		Assert.assertEquals("value2", pp.getProperty("id1"));
+	}
+
+	@Test
+	public void testP3SystemProperties() throws IOException {
+		ParametrizedProperties.setUseSystemProperties(true);
+		System.setProperty("id1", "system");
+		pp.load(ParametrizedProperties.class.getResourceAsStream(path+"p3.properties"));
+		Assert.assertEquals("system", pp.getProperty("id1"));
+		Assert.assertEquals("id2.value", pp.getProperty("id2"));
+	}
+}
