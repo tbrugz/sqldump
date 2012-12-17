085a5c7031d0f5243bd0bfd1dff38bd1351e724f
tbrugz <none@none>
2012-12-17 14:46:30 +0000

    test: added FailoverTest.testSimple()

--- src_test/tbrugz/sqldump/sqlrun/FailoverTest.java
+++ src_test/tbrugz/sqldump/sqlrun/FailoverTest.java
@@ -4,9 +4,19 @@ import org.junit.Test;
 
 public class FailoverTest {
 	@Test
-	public void doRun() throws Exception {
+	public void testAccessLog() throws Exception {
 		String[] params = {
-				"-propfile=src_test/tbrugz/sqldump/sqlrun/failover-access_logs.properties"
+				"-propfile=src_test/tbrugz/sqldump/sqlrun/failover-access_logs.properties",
+				"-usesysprop=false"
+		};
+		SQLRun.main(params);
+	}
+
+	@Test
+	public void testSimple() throws Exception {
+		String[] params = {
+				"-propfile=src_test/tbrugz/sqldump/sqlrun/failover-simple.properties",
+				"-usesysprop=false"
 		};
 		SQLRun.main(params);
 	}
