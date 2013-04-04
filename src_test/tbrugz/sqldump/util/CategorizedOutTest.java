39fbdbb84b4c4c1c8a4e1f2d957e71fae570333a
tbrugz <none@none>
2013-04-04 12:35:13 -0300

    test: CategorizedOutTest: more tests ; removed old SQLDumpTestSuite

--- src_test/tbrugz/sqldump/util/CategorizedOutTest.java
+++ src_test/tbrugz/sqldump/util/CategorizedOutTest.java
@@ -9,8 +9,11 @@ import org.junit.Test;
 public class CategorizedOutTest {
 
 	@Test
-	public void test1() throws IOException {
-		String outpattern = CategorizedOut.generateFinalOutPattern("work/output/test/cout-[ab]-[cd].txt", "ab", "cd");
+	public void testSimplePatternAndOutput() throws IOException {
+		String outpattern = CategorizedOut.generateFinalOutPattern("work/output/test/cout-[ab]-[cd].txt",
+				"[ab]", "[cd]");
+		Assert.assertEquals("work/output/test/cout-[1]-[2].txt", outpattern);
+		
 		CategorizedOut cout = new CategorizedOut(outpattern);
 		
 		cout.categorizedOut("message1 for x, y", "x", "y");
@@ -22,5 +25,25 @@ public class CategorizedOutTest {
 		
 		String m2 = IOUtil.readFromFilename("work/output/test/cout-x-y.txt");
 		Assert.assertEquals("message1 for x, y\nmessage2 for x, y\n", m2);
+	}
+
+	@Test
+	public void testArrayPattern() throws IOException {
+		String outpattern = CategorizedOut.generateFinalOutPattern("work/output/test/cout-[ab]-[cd].txt",
+				new String[]{"[ab]", "[ac]"}, new String[]{"[ca]", "[cd]"});
+		Assert.assertEquals("work/output/test/cout-[1]-[2].txt", outpattern);
+	}
+
+	@Test
+	public void testDeprecatedPattern() throws IOException {
+		String outpattern = CategorizedOut.generateFinalOutPattern("work/output/test/cout-${ab}.txt",
+				"${ab}");
+		//System.out.println("out: "+outpattern);
+		Assert.assertEquals("work/output/test/cout-[1].txt", outpattern);
+
+		String outpatternArr = CategorizedOut.generateFinalOutPattern("work/output/test/cout-${abc}.txt",
+				new String[]{"${q}", "${w}"},
+				new String[]{"${abc}", "${123}"});
+		Assert.assertEquals("work/output/test/cout-[2].txt", outpatternArr);
 	}
 }
