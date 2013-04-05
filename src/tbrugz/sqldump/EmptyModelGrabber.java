2677dfe9d3a304b8c4ea390d687fac199586a4c0
tbrugz <none@none>
2013-04-05 13:21:29 -0300

    grabbers: added EmptyModelGrabber

---
+++ src/tbrugz/sqldump/EmptyModelGrabber.java
@@ -0,0 +1,42 @@
+package tbrugz.sqldump;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+import tbrugz.sqldump.dbmodel.SchemaModel;
+import tbrugz.sqldump.def.SchemaModelGrabber;
+
+public class EmptyModelGrabber implements SchemaModelGrabber {
+
+	@Override
+	public void procProperties(Properties prop) {
+	}
+
+	@Override
+	public void setPropertiesPrefix(String propertiesPrefix) {
+	}
+
+	@Override
+	public boolean needsConnection() {
+		return false;
+	}
+
+	@Override
+	public Connection getConnection() {
+		return null;
+	}
+
+	@Override
+	public void setConnection(Connection conn) {
+	}
+
+	@Override
+	public void setFailOnError(boolean failonerror) {
+	}
+
+	@Override
+	public SchemaModel grabSchema() {
+		return new SchemaModel();
+	}
+
+}
