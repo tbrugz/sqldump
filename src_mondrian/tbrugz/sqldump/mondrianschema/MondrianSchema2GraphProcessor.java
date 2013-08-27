e77f6471b39bbc945aa90e9846c51d4dbd7bf892
tbrugz <none@none>
2013-08-27 18:48:40 -0300

    mondrian: schema2graph: added prop 'sqldump.mondrianschema2graph.xsl.simple'

--- src_mondrian/tbrugz/sqldump/mondrianschema/MondrianSchema2GraphProcessor.java
+++ src_mondrian/tbrugz/sqldump/mondrianschema/MondrianSchema2GraphProcessor.java
@@ -15,11 +15,16 @@ import tbrugz.sqldump.util.Utils;
 
 public class MondrianSchema2GraphProcessor extends XSLTProcessor {
 	
-	static Log log = LogFactory.getLog(MondrianSchema2GraphProcessor.class);
+	static final Log log = LogFactory.getLog(MondrianSchema2GraphProcessor.class);
 
 	public static final String PREFIX_MONDRIAN_2GRAPH = "sqldump.mondrianschema2graph";
+	
 	public static final String SUFFIX_INFILE = ".infile";
 	public static final String SUFFIX_OUTFILE = ".outfile";
+	public static final String SUFFIX_XSL_SIMPLE = ".xsl.simple";
+	
+	public static final String XSL_DEFAULT = "mondrianschema2graphml.xsl";
+	public static final String XSL_SIMPLE = "mondrianschema2graphml-simple.xsl";
 	
 	String schemaFile = null;
 	String graphOutFile = null;
@@ -34,17 +39,23 @@ public class MondrianSchema2GraphProcessor extends XSLTProcessor {
 		}
 		graphOutFile = prop.getProperty(PREFIX_MONDRIAN_2GRAPH+SUFFIX_OUTFILE);
 		
-		xsl = MondrianSchema2GraphProcessor.class.getResourceAsStream("mondrianschema2graphml.xsl");
+		if(Utils.getPropBool(prop, PREFIX_MONDRIAN_2GRAPH+SUFFIX_XSL_SIMPLE)) {
+			xsl = MondrianSchema2GraphProcessor.class.getResourceAsStream(XSL_SIMPLE);
+			log.info("simple XSL selected (no nodes for hierarchies)");
+		}
+		else {
+			xsl = MondrianSchema2GraphProcessor.class.getResourceAsStream(XSL_DEFAULT);
+		}
 	}
 	
 	@Override
 	public void process() {
 		if(schemaFile==null) {
-			log.warn("suffix '"+SUFFIX_INFILE+"' nor prop '"+MondrianSchemaDumper.PROP_MONDRIAN_SCHEMA_OUTFILE+"' defined");
+			log.warn("suffix '"+SUFFIX_INFILE+"' nor prop '"+MondrianSchemaDumper.PROP_MONDRIAN_SCHEMA_OUTFILE+"' defined [prefix is '"+PREFIX_MONDRIAN_2GRAPH+"']");
 			return;
 		}
 		if(graphOutFile==null) {
-			log.warn("suffix '"+SUFFIX_OUTFILE+"' not defined");
+			log.warn("suffix '"+SUFFIX_OUTFILE+"' not defined [prefix is '"+PREFIX_MONDRIAN_2GRAPH+"']");
 			return;
 		}
 		
