f6eec3427e23373130607bb71c0410d5c64967f1
tbrugz <none@none>
2013-10-11 19:04:23 -0300

    pivot: added AbstractConnectionDecorator

--- src_pivot/tbrugz/sqldump/pivot/PivotConnection.java
+++ src_pivot/tbrugz/sqldump/pivot/PivotConnection.java
@@ -1,44 +1,22 @@
 package tbrugz.sqldump.pivot;
 
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.CallableStatement;
-import java.sql.Clob;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.NClob;
 import java.sql.PreparedStatement;
-import java.sql.SQLClientInfoException;
 import java.sql.SQLException;
-import java.sql.SQLWarning;
-import java.sql.SQLXML;
-import java.sql.Savepoint;
 import java.sql.Statement;
-import java.sql.Struct;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.Executor;
 
-public class PivotConnection implements Connection {
+import tbrugz.sqldump.jdbc.AbstractConnectionDecorator;
+
+public class PivotConnection extends AbstractConnectionDecorator {
 	
-	final Connection conn;
-
 	public PivotConnection(Connection conn) {
-		this.conn = conn;
+		super(conn);
 	}
 	
 	void debug(String message) {
 		//System.out.println(message);
 	}
 	
-	public <T> T unwrap(Class<T> iface) throws SQLException {
-		return conn.unwrap(iface);
-	}
-
-	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-		return conn.isWrapperFor(iface);
-	}
-
 	//TODO
 	public Statement createStatement() throws SQLException {
 		debug("PivotConnection.createStatement()");
@@ -49,75 +27,6 @@ public class PivotConnection implements Connection {
 	public PreparedStatement prepareStatement(String sql) throws SQLException {
 		debug("PivotConnection.prepareStatement(sql)");
 		return new PivotPreparedStatement(conn.prepareStatement(sql), sql);
-	}
-
-	public CallableStatement prepareCall(String sql) throws SQLException {
-		return conn.prepareCall(sql);
-	}
-
-	//???
-	public String nativeSQL(String sql) throws SQLException {
-		return conn.nativeSQL(sql);
-	}
-
-	public void setAutoCommit(boolean autoCommit) throws SQLException {
-		conn.setAutoCommit(autoCommit);
-	}
-
-	public boolean getAutoCommit() throws SQLException {
-		return conn.getAutoCommit();
-	}
-
-	public void commit() throws SQLException {
-		conn.commit();
-	}
-
-	public void rollback() throws SQLException {
-		conn.rollback();
-	}
-
-	public void close() throws SQLException {
-		conn.close();
-	}
-
-	public boolean isClosed() throws SQLException {
-		return conn.isClosed();
-	}
-
-	public DatabaseMetaData getMetaData() throws SQLException {
-		return conn.getMetaData();
-	}
-
-	public void setReadOnly(boolean readOnly) throws SQLException {
-		conn.setReadOnly(readOnly);
-	}
-
-	public boolean isReadOnly() throws SQLException {
-		return conn.isReadOnly();
-	}
-
-	public void setCatalog(String catalog) throws SQLException {
-		conn.setCatalog(catalog);
-	}
-
-	public String getCatalog() throws SQLException {
-		return conn.getCatalog();
-	}
-
-	public void setTransactionIsolation(int level) throws SQLException {
-		conn.setTransactionIsolation(level);
-	}
-
-	public int getTransactionIsolation() throws SQLException {
-		return conn.getTransactionIsolation();
-	}
-
-	public SQLWarning getWarnings() throws SQLException {
-		return conn.getWarnings();
-	}
-
-	public void clearWarnings() throws SQLException {
-		conn.clearWarnings();
 	}
 
 	//TODO
@@ -134,43 +43,6 @@ public class PivotConnection implements Connection {
 		return new PivotPreparedStatement(conn.prepareStatement(sql, resultSetType, resultSetConcurrency), sql);
 	}
 
-	public CallableStatement prepareCall(String sql, int resultSetType,
-			int resultSetConcurrency) throws SQLException {
-		return conn.prepareCall(sql, resultSetType, resultSetConcurrency);
-	}
-
-	public Map<String, Class<?>> getTypeMap() throws SQLException {
-		return conn.getTypeMap();
-	}
-
-	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
-		conn.setTypeMap(map);
-	}
-
-	public void setHoldability(int holdability) throws SQLException {
-		conn.setHoldability(holdability);
-	}
-
-	public int getHoldability() throws SQLException {
-		return conn.getHoldability();
-	}
-
-	public Savepoint setSavepoint() throws SQLException {
-		return conn.setSavepoint();
-	}
-
-	public Savepoint setSavepoint(String name) throws SQLException {
-		return conn.setSavepoint(name);
-	}
-
-	public void rollback(Savepoint savepoint) throws SQLException {
-		conn.rollback(savepoint);
-	}
-
-	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
-		conn.releaseSavepoint(savepoint);
-	}
-
 	//TODO
 	public Statement createStatement(int resultSetType,
 			int resultSetConcurrency, int resultSetHoldability)
@@ -185,13 +57,6 @@ public class PivotConnection implements Connection {
 			throws SQLException {
 		System.out.println("PivotConnection.prepareStatement(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability)");
 		return new PivotPreparedStatement(conn.prepareStatement(sql, resultSetType, resultSetConcurrency, resultSetHoldability), sql);
-	}
-
-	public CallableStatement prepareCall(String sql, int resultSetType,
-			int resultSetConcurrency, int resultSetHoldability)
-			throws SQLException {
-		return conn.prepareCall(sql, resultSetType, resultSetConcurrency,
-				resultSetHoldability);
 	}
 
 	//TODO
@@ -215,73 +80,4 @@ public class PivotConnection implements Connection {
 		return new PivotPreparedStatement(conn.prepareStatement(sql, columnNames), sql);
 	}
 
-	public Clob createClob() throws SQLException {
-		return conn.createClob();
-	}
-
-	public Blob createBlob() throws SQLException {
-		return conn.createBlob();
-	}
-
-	public NClob createNClob() throws SQLException {
-		return conn.createNClob();
-	}
-
-	public SQLXML createSQLXML() throws SQLException {
-		return conn.createSQLXML();
-	}
-
-	public boolean isValid(int timeout) throws SQLException {
-		return conn.isValid(timeout);
-	}
-
-	public void setClientInfo(String name, String value)
-			throws SQLClientInfoException {
-		conn.setClientInfo(name, value);
-	}
-
-	public void setClientInfo(Properties properties)
-			throws SQLClientInfoException {
-		conn.setClientInfo(properties);
-	}
-
-	public String getClientInfo(String name) throws SQLException {
-		return conn.getClientInfo(name);
-	}
-
-	public Properties getClientInfo() throws SQLException {
-		return conn.getClientInfo();
-	}
-
-	public Array createArrayOf(String typeName, Object[] elements)
-			throws SQLException {
-		return conn.createArrayOf(typeName, elements);
-	}
-
-	public Struct createStruct(String typeName, Object[] attributes)
-			throws SQLException {
-		return conn.createStruct(typeName, attributes);
-	}
-
-	public void setSchema(String schema) throws SQLException {
-		conn.setSchema(schema);
-	}
-
-	public String getSchema() throws SQLException {
-		return conn.getSchema();
-	}
-
-	public void abort(Executor executor) throws SQLException {
-		conn.abort(executor);
-	}
-
-	public void setNetworkTimeout(Executor executor, int milliseconds)
-			throws SQLException {
-		conn.setNetworkTimeout(executor, milliseconds);
-	}
-
-	public int getNetworkTimeout() throws SQLException {
-		return conn.getNetworkTimeout();
-	}
-	
 }
