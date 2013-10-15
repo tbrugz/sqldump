199daaf53c8e3a27a435a6af2e1a6a0bc2533ded
tbrugz <none@none>
2013-10-15 12:24:28 -0300

    mondrian/olap4j: CellSetResultSetAdapter: 1st (working) version

--- src_mondrian/tbrugz/sqldump/mondrianschema/CellSetResultSetAdapter.java
+++ src_mondrian/tbrugz/sqldump/mondrianschema/CellSetResultSetAdapter.java
@@ -9,10 +9,12 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.olap4j.Axis;
+import org.olap4j.Cell;
 import org.olap4j.CellSet;
 import org.olap4j.CellSetAxis;
 import org.olap4j.Position;
 import org.olap4j.metadata.Hierarchy;
+import org.olap4j.metadata.Member;
 
 import tbrugz.sqldump.resultset.AbstractResultSet;
 import tbrugz.sqldump.resultset.RSMetaDataAdapter;
@@ -21,18 +23,31 @@ public class CellSetResultSetAdapter extends AbstractResultSet implements Result
 
 	static final Log log = LogFactory.getLog(CellSetResultSetAdapter.class);
 
+	// CellSet properties
 	final CellSet cellset;
-
+	final CellSetAxis rowAxis;
+	final CellSetAxis colAxis;
+	
+	// ResultSet properties
+	final int colsFromRowAxis;
 	final ResultSetMetaData metadata;
 	
 	public CellSetResultSetAdapter(CellSet cellset) {
 		this.cellset = cellset;
 		
 		List<CellSetAxis> axis = cellset.getAxes();
-		CellSetAxis rowAxis = getAxisOfType(Axis.ROWS, axis);
+		rowAxis = getAxisOfType(Axis.ROWS, axis);
 		if(rowAxis==null) { throw new IllegalStateException("ROWS Axis must not be null"); }
-		CellSetAxis colAxis = getAxisOfType(Axis.COLUMNS, axis);
+		colAxis = getAxisOfType(Axis.COLUMNS, axis);
 		if(colAxis==null) { throw new IllegalStateException("COLUMNS Axis must not be null"); }
+		
+		if(axis.size()>2) {
+			List<String> axisTypes = new ArrayList<String>(); 
+			for(CellSetAxis a: axis) {
+				axisTypes.add(a.getAxisMetaData().getAxisOrdinal().toString());
+			}
+			log.warn("more than 2 axis detected [#"+axis.size()+"]: "+axisTypes);
+		}
 		
 		List<String> colNames = new ArrayList<String>();
 		
@@ -46,6 +61,9 @@ public class CellSetResultSetAdapter extends AbstractResultSet implements Result
 		}
 		
 		metadata = new RSMetaDataAdapter(null, null, colNames);
+		
+		rowCount = rowAxis.getPositionCount();
+		colsFromRowAxis = rowAxis.getAxisMetaData().getHierarchies().size();
 	}
 	
 	static CellSetAxis getAxisOfType(Axis axisType, List<CellSetAxis> axis) {
@@ -61,4 +79,103 @@ public class CellSetResultSetAdapter extends AbstractResultSet implements Result
 	public ResultSetMetaData getMetaData() throws SQLException {
 		return metadata;
 	}
+
+	//--------------- navigation methods
+	
+	int position = -1;
+	int rowCount = 0;
+	
+	@Override
+	public void beforeFirst() throws SQLException {
+		resetPosition();
+	}
+	
+	@Override
+	public boolean first() throws SQLException {
+		resetPosition();
+		return next();
+	}
+	
+	@Override
+	public boolean absolute(int row) throws SQLException {
+		if(rowCount>=row) {
+			position = row-1;
+			updateCurrentElement();
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean relative(int rows) throws SQLException {
+		int newpos = position + rows + 1;
+		if(newpos>0) { return absolute(newpos); }
+		return false;
+	}
+
+	@Override
+	public boolean next() throws SQLException {
+		if(rowCount-1 > position) {
+			position++;
+			updateCurrentElement();
+			return true;
+		}
+		return false;
+	}
+	
+	void updateCurrentElement() {
+		/*if(position<0) {
+			currentNonPivotKey = null;
+			return;
+		}
+		currentNonPivotKey = nonPivotKeyValues.get(position);*/
+	}
+	
+	void resetPosition() {
+		position = -1;
+		updateCurrentElement();
+	}
+	
+	//--------------- value getter methods
+	
+	@Override
+	public Object getObject(int columnIndex) throws SQLException {
+		//log.info("getObject: "+columnIndex +" "+ colsFromRowAxis); 
+		if(columnIndex <= colsFromRowAxis) {
+			//Hierarchy hier = rowAxis.getAxisMetaData().getHierarchies().get(columnIndex-1);
+			//List<Member> members = rowAxis.getPositions().get(position).getMembers();
+			//log.info("members["+position+";"+columnIndex+"]: "+members);
+			Member member = rowAxis.getPositions().get(position).getMembers().get(columnIndex-1);
+			return member.getCaption();
+		}
+		Position rowP = rowAxis.getPositions().get(position);
+		int rowOrdinal = rowP.getOrdinal();
+		Position colP = colAxis.getPositions().get(columnIndex-colsFromRowAxis-1);
+		int colOrdinal = colP.getOrdinal();
+		
+		try {
+			Cell c = cellset.getCell(colP, rowP);
+			return c.getValue();
+		}
+		catch(IndexOutOfBoundsException e) {
+			log.warn("error["+rowOrdinal+"/"+colOrdinal+"]: "+e);
+			return null;
+			//return rowOrdinal+"/"+colOrdinal;
+		}
+	}
+	
+	@Override
+	public String getString(String columnLabel) throws SQLException {
+		Object o = getObject(columnLabel);
+		if(o==null) { return null; }
+		return String.valueOf(o);
+	}
+
+	@Override
+	public String getString(int columnIndex) throws SQLException {
+		Object o = getObject(columnIndex);
+		if(o==null) { return null; }
+		return String.valueOf(o);
+	}
+	
 }
