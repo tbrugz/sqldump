48d9ac4ffd78cef0dcb96755f922055cc73208d5
tbrugz <none@none>
2014-02-26 16:33:02 -0300

    processor: SQLTests: added main()

--- src/tbrugz/sqldump/SQLTests.java
+++ src/tbrugz/sqldump/SQLTests.java
@@ -1,12 +1,23 @@
 package tbrugz.sqldump;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import tbrugz.sqldump.def.AbstractSQLProc;
+import tbrugz.sqldump.util.CLIProcessor;
+import tbrugz.sqldump.util.ConnectionUtil;
+import tbrugz.sqldump.util.ParametrizedProperties;
+import tbrugz.sqldump.util.SQLUtils;
 
 public class SQLTests extends AbstractSQLProc {
 
@@ -56,6 +67,30 @@ public class SQLTests extends AbstractSQLProc {
 	@Override
 	public void process() {
 		tests(conn);
+	}
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, NamingException {
+		if(args.length!=2) {
+			String message = "should have 2 arguments: properties file & connection properties prefix";
+			log.warn(message);
+			throw new RuntimeException(message);
+		}
+		SQLTests st = new SQLTests();
+		st.doMain(args[0], args[1]);
+	}
+	
+	void doMain(String fileName, String connPrefix) throws ClassNotFoundException, SQLException, NamingException, FileNotFoundException, IOException {
+		log.info("loading props: "+fileName);
+		File f = new File(fileName);
+
+		ParametrizedProperties p = new ParametrizedProperties();
+		p.setProperty(CLIProcessor.PROP_PROPFILEBASEDIR, f.getParent());
+		p.load(new FileInputStream(f));
+		log.info("initing conn: "+connPrefix);
+		Connection conn = ConnectionUtil.initDBConnection(connPrefix, p);
+		
+		setConnection(conn);
+		process();
 	}
 	
 }
