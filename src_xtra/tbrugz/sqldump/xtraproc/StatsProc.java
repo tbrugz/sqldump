f6dd1c33d074b65aa5a5929de392fc26e0cbaf4a
tbrugz <none@none>
2015-12-10 11:13:47 -0200

    processor: StatsProc: added props '.outfilepattern' & '.x-order'

--- src_xtra/tbrugz/sqldump/xtraproc/StatsProc.java
+++ src_xtra/tbrugz/sqldump/xtraproc/StatsProc.java
@@ -1,10 +1,13 @@
 package tbrugz.sqldump.xtraproc;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -12,22 +15,53 @@ import org.apache.commons.logging.LogFactory;
 import tbrugz.sqldump.dbmodel.Table;
 import tbrugz.sqldump.def.AbstractSQLProc;
 import tbrugz.sqldump.def.ProcessingException;
+import tbrugz.sqldump.util.CategorizedOut;
 import tbrugz.sqldump.util.MapEntryValueComparator;
 
 /*
  * should this be a "data profiler"? 
- * see: http://wiki.pentaho.com/display/EAI/Kettle+Data+Profiling+with+DataCleaner
+ * see: http://wiki.pentaho.com/display/EAI/Kettle+Data+Profiling+with+DataCleaner , https://en.wikipedia.org/wiki/Data_profiling
  * 
- * TODO: use tbrugz.sqldump.util.CategorizedOut ?
+ * XXX: CategorizedOut: use [schemaname]?
  */
 public class StatsProc extends AbstractSQLProc {
 
 	static final Log log = LogFactory.getLog(StatsProc.class);
+
+	static final String PROP_PREFIX = "sqldump.statsproc";
+
+	static final String PROP_OUTFILEPATTERN = PROP_PREFIX+".outfilepattern";
+	static final String PROP_MAP_ORDER = PROP_PREFIX+".x-order";
+	
+	String fileOutput;
+	boolean orderByValue = false;
+	
+	@Override
+	public void setProperties(Properties prop) {
+		super.setProperties(prop);
+		fileOutput = prop.getProperty(PROP_OUTFILEPATTERN, CategorizedOut.STDOUT);
+		orderByValue = "value".equalsIgnoreCase(prop.getProperty(PROP_MAP_ORDER));
+	}
 	
 	@Override
 	public void process() {
+		try {
+			processIntern();
+		}
+		catch(IOException e) {
+			log.warn("StatsProc error: "+e);
+			if(failonerror) {
+				throw new ProcessingException(e);
+			}
+		}
+	}
+	
+	void processIntern() throws IOException {
 		Map<String,Integer> map = new HashMap<String,Integer>();
 
+		//String finalPattern = CategorizedOut.generateFinalOutPattern(fileOutput, (String[]) null);
+		CategorizedOut cout = new CategorizedOut(fileOutput);
+		
 		try {
 			for(Table table: model.getTables()) {
 				ResultSet rs = conn.createStatement().executeQuery("select count(*) from "+table.getFinalQualifiedName());
@@ -44,15 +78,17 @@ public class StatsProc extends AbstractSQLProc {
 			}
 		}
 		
-		/*for (String key: map.keySet()) {
-			System.out.println("map key/value: " + key + "/"+map.get(key));
-		}*/
-
-		Map<String,Integer> sorted_map = MapEntryValueComparator.sortByValue(map, true);
+		Map<String,Integer> sorted_map = orderByValue?
+				MapEntryValueComparator.sortByValue(map, true) :
+				new TreeMap<String, Integer>(map);
 		
+		int count = 0;
 		for(Entry<String,Integer> entry: sorted_map.entrySet()) {
-			System.out.println("count["+entry.getKey()+"]: "+entry.getValue());
+			//System.out.println("count["+entry.getKey()+"]: "+entry.getValue());
+			cout.categorizedOut(entry.getKey()+": count = "+entry.getValue());
+			count++;
 		}
+		log.info("stats for "+count+" tables dumped, outpattern: "+fileOutput);
 	}
 
 }
