95ebf44bebbbec85d7ce9a416a5d9d158955e453
tbrugz <none@none>
2016-05-10 16:07:08 -0300

    util: StringUtils: added readInputStream()

--- src/tbrugz/sqldump/util/StringUtils.java
+++ src/tbrugz/sqldump/util/StringUtils.java
@@ -1,5 +1,9 @@
 package tbrugz.sqldump.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -29,5 +33,18 @@ public class StringUtils {
 		return ret;
 	}
 	
+	// http://stackoverflow.com/a/309718/616413
+	public static String readInputStream(final InputStream is, final int bufferSize) throws IOException {
+		final char[] buffer = new char[bufferSize];
+		final StringBuilder out = new StringBuilder();
+		Reader in = new InputStreamReader(is, "UTF-8");
+		for (;;) {
+			int rsz = in.read(buffer, 0, buffer.length);
+			if (rsz < 0)
+				break;
+			out.append(buffer, 0, rsz);
+		}
+		return out.toString();
+	}
 
 }
