ef76bb88fed75ea311a744497120543354c14371
Telmo Brugnara <tbrugz@gmail.com>
2021-06-03 00:12:37 -0300

    sqlrun: tokenizer: added TokenizerUtil.getNamedParameters()

--- src_run/tbrugz/sqldump/sqlrun/tokenzr/TokenizerUtil.java
+++ src_run/tbrugz/sqldump/sqlrun/tokenzr/TokenizerUtil.java
@@ -1,15 +1,28 @@
 package tbrugz.sqldump.sqlrun.tokenzr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TokenizerUtil {
+
+	public static class QueryParameter {
+		final String name;
+		final int position;
+
+		public QueryParameter(String name, int position) {
+			this.name = name;
+			this.position = position;
+		}
+	}
 
 	public static boolean containsSqlStatmement(String sql) {
 		return removeSqlComents(sql).trim().length()>0;
 	}
 	
 	enum TknState {
-		DEFAULT, STRING, DQUOTE, COMM_LINE, COMM_BLOCK
+		DEFAULT, STRING, DQUOTE, /*NAMEDPAR,*/ COMM_LINE, COMM_BLOCK
 	}
-	
+
 	public static String removeSqlComents(String sql) {
 		TknState state = TknState.DEFAULT;
 		StringBuilder ret = new StringBuilder();
@@ -85,4 +98,85 @@ public class TokenizerUtil {
 		//System.out.println("["+sret+"]");
 		return ret.toString();
 	}
+
+	/**
+	 * Return named parameters - defined as <code>:[a-zA-Z_][0-9a-zA-Z_]*</code>. Ex: <code>:someParam</code>
+	 */
+	public static List<QueryParameter> getNamedParameters(String sql) {
+		TknState state = TknState.DEFAULT;
+		List<QueryParameter> ret = new ArrayList<QueryParameter>();
+
+		int len = sql.length();
+		for(int i=0;i<len;i++) {
+			char c = sql.charAt(i);
+			boolean lastChar = (i==len-1);
+			switch (state) {
+				case DEFAULT: {
+					if(c=='\'') {
+						state = TknState.STRING;
+					}
+					else if(c=='"') {
+						state = TknState.DQUOTE;
+					}
+					else if(c=='-' && !lastChar) {
+						char c2 = sql.charAt(i+1);
+						if(c2=='-') { state = TknState.COMM_LINE; }
+					}
+					else if(c=='/' && !lastChar) {
+						char c2 = sql.charAt(i+1);
+						if(c2=='*') { state = TknState.COMM_BLOCK; }
+					}
+					else if(c==':' && !lastChar) {
+						StringBuilder sb = new StringBuilder();
+						int pos = i;
+						char cp = sql.charAt(++i);
+						if( (cp>='A' && cp<='Z') || (cp>='a' && cp<='z') || cp == '_' ) {
+							while( (cp>='A' && cp<='Z') || (cp>='a' && cp<='z') || (cp>='0' && cp<='9') || cp == '_') {
+								sb.append(cp);
+								if(i==len-1) { break; }
+								cp = sql.charAt(++i);
+							}
+						}
+						QueryParameter qp = new QueryParameter(sb.toString(), pos);
+						ret.add(qp);
+					}
+					/*else if(c=='?') {
+						// positional param?
+					}*/
+					break;
+				}
+				case STRING: {
+					if(c=='\'') {
+						state = TknState.DEFAULT;
+					}
+					break;
+				}
+				case DQUOTE: {
+					if(c=='"') {
+						state = TknState.DEFAULT;
+					}
+					break;
+				}
+				case COMM_LINE: {
+					if(c=='\n') {
+						state = TknState.DEFAULT;
+					}
+					break;
+				}
+				case COMM_BLOCK: {
+					if(c=='*' && !lastChar) {
+						char c2 = sql.charAt(i+1);
+						if(c2=='/') {
+							state = TknState.DEFAULT;
+							i++; // skips next '/' char
+						}
+					}
+					break;
+				}
+			}
+		}
+	
+		return ret;
+	}
+
 }
--- src_test/tbrugz/sqldump/sqlrun/tokenzr/TokenizerUtilTest.java
+++ src_test/tbrugz/sqldump/sqlrun/tokenzr/TokenizerUtilTest.java
@@ -1,7 +1,11 @@
 package tbrugz.sqldump.sqlrun.tokenzr;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import tbrugz.sqldump.sqlrun.tokenzr.TokenizerUtil.QueryParameter;
 
 public class TokenizerUtilTest {
 
@@ -71,6 +75,117 @@ public class TokenizerUtilTest {
 
 		test = "select \" -- \n\" 2";
 		Assert.assertEquals("select \" -- \n\" 2", TokenizerUtil.removeSqlComents(test));
+	}
+
+	@Test
+	public void testNamedParameters() {
+		String test = null;
+		List<QueryParameter> qpl = null;
+
+		test = "select :abc ";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+
+		test = "select :abc";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+	}
+
+	@Test
+	public void test2NamedParameters() {
+		String test = null;
+		List<QueryParameter> qpl = null;
+
+		test = "select :abc :def";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(2, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+		Assert.assertEquals("def", qpl.get(1).name);
+		Assert.assertEquals(12, qpl.get(1).position);
+
+		test = "select :defg :defg";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(2, qpl.size());
+		Assert.assertEquals("defg", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+		Assert.assertEquals("defg", qpl.get(1).name);
+		Assert.assertEquals(13, qpl.get(1).position);
+	}
+
+	@Test
+	public void testNamedParametersInComm() {
+		String test = null;
+		List<QueryParameter> qpl = null;
+
+		test = "select :abc --:def";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+
+		test = "select --:abc \n :def";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("def", qpl.get(0).name);
+		Assert.assertEquals(16, qpl.get(0).position);
+	}
+
+	@Test
+	public void testNamedParametersInBlockComm() {
+		String test = null;
+		List<QueryParameter> qpl = null;
+
+		test = "select :abc /* :def */";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+
+		test = "select /*:abc*/  :def";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("def", qpl.get(0).name);
+		Assert.assertEquals(17, qpl.get(0).position);
+	}
+
+	@Test
+	public void testNamedParametersInString() {
+		String test = null;
+		List<QueryParameter> qpl = null;
+
+		test = "select :abc ':def' ";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+
+		test = "select ':abc'  :def";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("def", qpl.get(0).name);
+		Assert.assertEquals(15, qpl.get(0).position);
+	}
+
+	@Test
+	public void testNamedParametersDQuotes() {
+		String test = null;
+		List<QueryParameter> qpl = null;
+
+		test = "select :abc \":def\" ";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("abc", qpl.get(0).name);
+		Assert.assertEquals(7, qpl.get(0).position);
+
+		test = "select \":abc\"  :def";
+		qpl = TokenizerUtil.getNamedParameters(test);
+		Assert.assertEquals(1, qpl.size());
+		Assert.assertEquals("def", qpl.get(0).name);
+		Assert.assertEquals(15, qpl.get(0).position);
 	}
 
 }
