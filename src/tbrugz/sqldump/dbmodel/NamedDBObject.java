d6714ec4c9dbfbb8e42076ba1c766282eecfcc0d
Telmo Brugnara <tbrugz@gmail.com>
2021-09-23 21:33:18 -0300

    dbmodel: added NamedDBObject.SchemaAndNameComparator

--- src/tbrugz/sqldump/dbmodel/NamedDBObject.java
+++ src/tbrugz/sqldump/dbmodel/NamedDBObject.java
@@ -1,5 +1,7 @@
 package tbrugz.sqldump.dbmodel;
 
+import java.util.Comparator;
+
 public interface NamedDBObject /* extends Comparable<NamedDBObject>? */ {
 
 	public String getName();
@@ -9,4 +11,30 @@ public interface NamedDBObject /* extends Comparable<NamedDBObject>? */ {
 	//public String getRemarks();
 	
 	//public default String getQualifiedName(); // java8
+
+	public static class SchemaAndNameComparator implements Comparator<NamedDBObject> {
+		@Override
+		public int compare(NamedDBObject o1, NamedDBObject o2) {
+			int comp = 0;
+			if(o1.getSchemaName()!=null) {
+				if(o2.getSchemaName()!=null) {
+					comp = o1.getSchemaName().compareTo(o2.getSchemaName());
+				}
+				else {
+					comp = 1;
+				}
+			}
+			else {
+				if(o2.getSchemaName()!=null) {
+					comp = -1;
+				}
+				//else { comp = 0; }
+			}
+			if(comp==0) {
+				comp = o1.getName().compareTo(o2.getName());
+			}
+			return comp;
+		}
+	}
+
 }
