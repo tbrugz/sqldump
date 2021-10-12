12dd173144ad00e609e4408e5f71e8c84be252c6
Telmo Brugnara <tbrugz@gmail.com>
2021-10-12 01:23:19 -0300

    datadump: xls/xlsx: better pivot headers

--- src/tbrugz/sqldump/datadump/PoiXlsSyntax.java
+++ src/tbrugz/sqldump/datadump/PoiXlsSyntax.java
@@ -12,16 +12,19 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import tbrugz.sqldump.datadump.DataDumpUtils.PivotHeaderCol;
+import tbrugz.sqldump.datadump.DataDumpUtils.PivotHeaderRow;
 import tbrugz.sqldump.util.SQLUtils;
 
 /*
  * HSSF: Excel XLS -- application/vnd.ms-excel
  * 
- * https://poi.apache.org/spreadsheet/quick-guide.html
+ * https://poi.apache.org/components/spreadsheet/quick-guide.html
  */
 public class PoiXlsSyntax extends OutputStreamDumper implements DumpSyntaxBuilder, Cloneable {
 
@@ -31,9 +34,12 @@ public class PoiXlsSyntax extends OutputStreamDumper implements DumpSyntaxBuilde
 	
 	public static final String MIME_TYPE = "application/vnd.ms-excel";
 	
+	public static final String UNICODE_NULL = "\u2400";
+	
 	Workbook wb;
 	Sheet sheet;
 	CellStyle cellDateStyle;
+	int numberOfHeaderRows = 1;
 	
 	@Override
 	public void procProperties(Properties prop) {
@@ -59,11 +65,47 @@ public class PoiXlsSyntax extends OutputStreamDumper implements DumpSyntaxBuilde
 	public void dumpHeader(OutputStream os) throws IOException {
 		wb = createWorkbook();
 		sheet = wb.createSheet(tableName);
-		Row row = sheet.createRow(0);
-		for(int i=0;i<numCol;i++) {
-			String s = lsColNames.get(i);
-			Cell cell = row.createCell(i);
-			cell.setCellValue(s);
+		
+		CellStyle cellHeaderStyle = getHeaderStyle(wb);
+		
+		PivotInfo pivotInfo = DataDumpUtils.guessPivotCols(lsColNames);
+		if(pivotInfo.isPivotResultSet()) {
+			CellStyle cellMeasureStyle = getMeasureStyle(wb);
+			List<PivotHeaderRow> rows = DataDumpUtils.getPivotedTableHeaderRows(pivotInfo, lsColNames);
+			for(int i=0;i<rows.size();i++) {
+				PivotHeaderRow phr = rows.get(i);
+				Row row = sheet.createRow(i);
+				for(int j=0;j<phr.rows.size();j++) {
+					Cell cell = row.createCell(j);
+					PivotHeaderCol phc = phr.rows.get(j);
+					if(phc.blank) {
+						//cell.setCellValue("<blank>");
+					}
+					else if(phc.isNull) {
+						cell.setCellValue(UNICODE_NULL);
+					}
+					else {
+						cell.setCellValue(phc.collabel);
+					}
+					
+					if(phc.measure) {
+						cell.setCellStyle(cellMeasureStyle);
+					}
+					else {
+						cell.setCellStyle(cellHeaderStyle);
+					}
+				}
+			}
+			numberOfHeaderRows = rows.size();
+		}
+		else {
+			Row row = sheet.createRow(0);
+			for(int i=0;i<numCol;i++) {
+				Cell cell = row.createCell(i);
+				String s = lsColNames.get(i);
+				cell.setCellValue(s);
+				cell.setCellStyle(cellHeaderStyle);
+			}
 		}
 		
 		CreationHelper createHelper = wb.getCreationHelper();
@@ -75,7 +117,7 @@ public class PoiXlsSyntax extends OutputStreamDumper implements DumpSyntaxBuilde
 	public void dumpRow(ResultSet rs, long count, OutputStream os)
 			throws IOException, SQLException {
 		List<Object> vals = SQLUtils.getRowObjectListFromRS(rs, lsColTypes, numCol, false); //XXX: internal resultSet as new sheet??
-		Row row = sheet.createRow((int) count+1);
+		Row row = sheet.createRow((int) count+numberOfHeaderRows);
 		for(int i=0;i<numCol;i++) {
 			Object o = vals.get(i);
 			Cell cell = row.createCell(i);
@@ -114,6 +156,22 @@ public class PoiXlsSyntax extends OutputStreamDumper implements DumpSyntaxBuilde
 	public void dumpFooter(long count, boolean hasMoreRows, OutputStream os) throws IOException {
 		wb.write(os);
 		wb.close();
+	}
+	
+	static CellStyle getHeaderStyle(Workbook wb) {
+		Font font = wb.createFont();
+		font.setBold(true);
+		CellStyle style = wb.createCellStyle();
+		style.setFont(font);
+		return style;
+	}
+
+	static CellStyle getMeasureStyle(Workbook wb) {
+		Font font = wb.createFont();
+		font.setItalic(true);
+		CellStyle style = wb.createCellStyle();
+		style.setFont(font);
+		return style;
 	}
 
 }
