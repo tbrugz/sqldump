6551e1b092d30f6f692604b65c0263f4b9e3074e
Telmo Brugnara <tbrugz@gmail.com>
2025-08-18 16:46:43 -0300

    pom: migrating javax.mail (1.4.7) -> jakarta.mail (2.0.1) - Jakarta EE 9

--- sqldump-core/src/main/java/tbrugz/sqldump/processors/SendMail.java
+++ sqldump-core/src/main/java/tbrugz/sqldump/processors/SendMail.java
@@ -2,12 +2,12 @@ package tbrugz.sqldump.processors;
 
 import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
