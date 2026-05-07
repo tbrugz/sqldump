5fbe54b664160db025ef0221889d71354be0fc73
Telmo Brugnara <tbrugz@gmail.com>
2026-05-06 23:09:26 -0300

    util: XMLSerializer: safer unmarshalling

--- sqldump-core/src/main/java/tbrugz/sqldump/util/XMLSerializer.java
+++ sqldump-core/src/main/java/tbrugz/sqldump/util/XMLSerializer.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
@@ -15,18 +19,33 @@ import jakarta.xml.bind.Unmarshaller;
 public class XMLSerializer {
 
 	final JAXBContext jc;
+	final XMLInputFactory xif;
 	
 	public XMLSerializer(String contextPath) throws JAXBException {
-		this.jc = JAXBContext.newInstance(contextPath, Thread.currentThread().getContextClassLoader());
+		this( JAXBContext.newInstance(contextPath, Thread.currentThread().getContextClassLoader()) );
 	}
 
 	public XMLSerializer(JAXBContext jc) throws JAXBException {
 		this.jc = jc;
+		xif = XMLInputFactory.newFactory();
+		xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+		xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
 	}
 	
 	public Object unmarshal(Reader reader) throws JAXBException {
+		/*
 		Unmarshaller u = jc.createUnmarshaller();
 		Object o = u.unmarshal(reader);
+		*/
+		// https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html#jaxb-unmarshaller
+		XMLStreamReader xsr = null;
+		try {
+			xsr = xif.createXMLStreamReader(reader);
+		} catch (XMLStreamException e) {
+			throw new RuntimeException(e);
+		}  
+		Unmarshaller um = jc.createUnmarshaller();
+		Object o = um.unmarshal(xsr);
 		//use Unmarshaller.afterUnmarshal()?
 		return o;
 	}
